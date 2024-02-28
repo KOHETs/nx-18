1cfbfe55a258ab7185a045e31150eee659c2faa0
KOHETs <kohet.s@gmail.com>
2024-02-28 21:33:19 +0900

    add @ngrx/store to angular

--- apps/angular/src/app/app.config.ts
+++ apps/angular/src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), provideAnimationsAsync()],
+  providers: [
+    provideStoreDevtools({ logOnly: !isDevMode() }),
+    provideEffects(),
+    provideStore(),
+    provideRouter(appRoutes),
+    provideAnimationsAsync(),
+  ],
 };
